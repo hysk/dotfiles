8464191133cfeb94e5e0ffa15b878f9d7a1bd698
YusukeHiguchiAppScan <higuchiyusuke@next-group.jp>
2014-07-29 18:17:29 +0900

    modify shell

--- createdotifileslink.sh
+++ createdotifileslink.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+
+echo "start"
+
+cd `dirname $0`
+
+targets=(.vimrc .vim)
+
+for file in ${targets[@]}; do
+    fpath=$HOME/$file
+    echo "$fpath"
+    if [ -a $fpath ]; then
+        echo "->already exists."
+    else
+        ln -s $PWD/$file $fpath
+        echo "->link created."
+    fi
+done
 
 
 
